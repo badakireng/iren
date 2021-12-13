08d673fe049a0f0dd225cfdd91d6f732a1c3e40b
badakireng <badakireng717@gmail.com>
2021-12-14 01:54:29 +0700

    iren

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8888 0888 8888 88888
+2. BCA 
+	ㅁ 8801 0887 4880 88888
+3. BNI 
+	ㅁ 881 0888 4880 88888
+4. PERMATA
+	ㅁ 852 8887 8888 88888
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0821 8235 4183
+2. OVO
+	ㅁ 0888 8888 8888
+3. DANA
+	ㅁ 0812 3775 5756
+	
+Semuanya atas nama y****a
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 83848
+2. BCA 
+	ㅁ 3901 0887 4350 93747
+3. BNI 
+	ㅁ 881 0887 4350 48888
+4. PERMATA
+	ㅁ 852 8887 4350 49497
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0821 8235 4183
+2. OVO
+	ㅁ 0887 4350 888887
+3. DANA
+	ㅁ 0812 3775 5756
+	
+Semuanya atas nama Y****A
+`
+	}
+
+exports.donasibot = donasibot
+ 
